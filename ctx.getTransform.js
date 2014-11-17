d8858453113e7aa5d709016300dc807c61a03edd
Elijah Insua <tmpvar@gmail.com>
2014-11-16 18:04:17 -0800

    initial implementation

---
+++ ctx.getTransform.js
@@ -0,0 +1,83 @@
+var mat3 = require('gl-matrix-mat3');
+
+module.exports = monkeyPatchCtxToAddGetTransform;
+
+function monkeyPatchCtxToAddGetTransform(ctx) {
+
+  var mat = mat3.create();
+  var stack = [];
+  var v2scratch = [0, 0, 0];
+  var m3scratch = mat3.create();
+
+  ctx.getTransform = function tGetTransform() {
+    return mat;
+  };
+
+  (function(save) {
+    ctx.save = function tSave(){
+      stack.push(mat3.clone(mat));
+      return save.call(ctx);
+    };
+  })(ctx.save);
+
+  (function(restore) {
+    ctx.restore = function tRestore(){
+      mat = stack.pop();
+      return restore.call(ctx);
+    };
+  })(ctx.restore);
+
+  (function(scale) {
+    ctx.scale = function tScale(sx, sy){
+      v2scratch[0] = sx;
+      v2scratch[1] = sy;
+      mat3.scale(mat, mat, v2scratch);
+      return scale.call(ctx, sx, sy);
+    };
+  })(ctx.scale);
+
+  (function(rotate) {
+    ctx.rotate = function tRotate(radians){
+      mat3.rotate(mat, mat, rads);
+      return rotate.call(ctx, rads);
+    };
+  })(ctx.rotate);
+
+  (function(translate) {
+    ctx.translate = function tTranslate(dx, dy){
+      v2scratch[0] = dx;
+      v2scratch[1] = dy;
+
+      mat3.translate(mat, mat, v2scratch);
+      return translate.call(ctx, dx, dy);
+    };
+  })(ctx.translate);
+
+  (function(transform) {
+    ctx.transform = function tTransform(a, b, c, d, e, f){
+      m3scratch[0] = a;
+      m3scratch[1] = c;
+      m3scratch[2] = e;
+      m3scratch[3] = b;
+      m3scratch[4] = d;
+      m3scratch[4] = f;
+
+      mat3.multiply(mat, math, m3scratch);
+      return transform.call(ctx, a, b, c, d, e, f);
+    };
+  })(ctx.transform);
+
+  (function(setTransform) {
+    ctx.setTransform = function tSetTransform(a, b, c, d, e, f){
+      mat[0] = a;
+      mat[1] = c;
+      mat[2] = e;
+      mat[3] = b;
+      mat[4] = d;
+      mat[4] = f;
+      return setTransform.call(ctx, a, b, c, d, e, f);
+    };
+  })(ctx.setTransform);
+
+  return ctx;
+}
---
+++ demo.js
@@ -0,0 +1,39 @@
+var mat3 = require('gl-matrix-mat3');
+var vec2 = require('gl-matrix-vec2');
+
+var mouse = vec2.create();
+var scale = 1;
+
+var v2scratch = vec2.create();
+var m3scratch = mat3.create();
+
+var ctx = require('fc')(function() {
+  ctx.fillStyle = "red";
+  ctx.clear();
+  ctx.save();
+    ctx.translate(window.innerWidth/2, window.innerHeight/2);
+    ctx.scale(scale, scale);
+
+    // compute where the mouse is in the scene
+    var mat = ctx.getTransform();
+    mat3.invert(m3scratch, mat)
+    vec2.transformMat3(v2scratch, mouse, m3scratch);
+
+    // draw the mouse!
+    ctx.fillRect(v2scratch[0] - 5, v2scratch[1] - 5, 10, 10);
+
+  ctx.restore();
+
+}, true);
+
+require('./ctx.getTransform')(ctx);
+
+document.addEventListener('mousemove', function(e) {
+  mouse[0] = e.x;
+  mouse[1] = e.y;
+});
+
+document.addEventListener('mousewheel', function(e) {
+  scale += e.wheelDeltaY / 100;
+  e.preventDefault();
+});
